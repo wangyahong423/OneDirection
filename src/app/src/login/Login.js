6ef6d4a79698a9eed71d2679c7e0fb7ebb4e5e0d
liuyanyansun <1694614518@qq.com>
2019-12-09 17:55:54 +0800

    message

--- src/app/src/login/Login.js
+++ src/app/src/login/Login.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
-import { List, TextareaItem, Button } from 'antd-mobile';
+import { List, TextareaItem,Button } from 'antd-mobile';
+import axios from 'axios';
 import { createForm } from 'rc-form';
 import { BrowserRouter as Router,Route,Link} from 'react-router-dom';
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        username: '',
+        pwd: ''
+    }
+}
+get=(e)=>{
+  this.setState({
+    username:e.target.value
+  })
+  console.log(e.target.value)
+}
+getFir=(e)=>{
+  this.setState({
+    pwd:e.target.value
+  })
+  console.log(e.target.value)
+}
+handleRegister = () => {
+    console.log(this.state.username,this.state.pwd);
+    let url = `http://localhost:3005/users/login?name=${this.state.username}&pwd=${this.state.pwd}`;
+    axios(url)
+        .then((res) => {
+            if (res.data.ok) {
+                alert(res.data.msg);
+                window.location.href='http://localhost:3000/login';
+            } else {
+                alert(res.data.msg);
+            }
+            console.log(res.data);
+        })
+        // .catch(error => alert("账户不存在！"));
+
+}
   render() {
     const { getFieldProps } = this.props.form;
     return (
@@ -13,40 +49,30 @@ class Login extends Component {
         <div style={{width:'100%',textAlign:'center',marginTop:'2vh'}}>
             <span style={{color:'#37376f',fontSize:25}}>校园新生通</span>
         </div>
-        <List renderHeader={() => ''} style={{marginTop:"2vh",width:'80%',marginLeft:'10%'}}>
-          <TextareaItem
-            {...getFieldProps('note4')}
-            placeholder="                       输入账号"
-            count={10}
-          />
-        </List>
+
+        <input placeholder="                                输入账号" style={{marginTop:"2vh",width:'80%',marginLeft:'10%',height:35,borderRadius:10}} onChange={this.get}/>
+        <input placeholder="                                输入密码" style={{marginTop:"3vh",width:'80%',marginLeft:'10%',height:35,borderRadius:10,}} onChange={this.getFir}/>
        
-        <List renderHeader={() => ''} style={{marginTop:"1vh",width:'80%',marginLeft:'10%'}}>
-          <TextareaItem
-            {...getFieldProps('note5')}
-            placeholder="                       输入密码"
-            count={100}
-          />
-        </List>
-        <Link to="/login">
-        <div style={{width:"70%",textAlign:'center',marginTop:'6vh',}}>
-            <Button style={{background:'#37376f',marginLeft:'40%'}}>
-                <span style={{color:'white',textAlign:'center'}}>登录</span>
+        
+        {/* <Link to="/login"> */}
+        <div style={{width:"70%",textAlign:'center',marginTop:'6vh',}} onClick={this.handleRegister} >
+            <Button style={{background:'#37376f',marginLeft:'40%'}} >
+                <span style={{color:'white',textAlign:'center'}} >登录</span>
             </Button>
         </div>
-        </Link>
+        {/* </Link> */}
         <div style={{width:'100%',marginTop:'8vh',color:'blue',textAlign:'center'}}>
             <span >忘记密码？</span>
             <Link to="/zhuce"><span style={{marginLeft:'35%',color:'blue'}}>没有账号  去注册？</span></Link>
         </div>
-        <div style={{marginTop:"3vh"}}>
+        {/* <div style={{marginTop:"3vh"}}>
             <img src={require('./img/denglu.jpg')} style={{width:'100%'}}/>
-        </div>
-        <div style={{width:'100%',textAlign:'center',marginTop:'5%'}}>
+        </div> */}
+        {/* <div style={{width:'100%',textAlign:'center',marginTop:'5%'}}>
             <span><img src={require('./img/weixin.jpg')} style={{width:'10vw',height:'10vw'}} /></span>
             <span><img src={require('./img/weibo.jpg')} style={{width:'10vw',height:'10vw',marginLeft:30}} /></span>
             <span><img src={require('./img/QQ.jpg')} style={{width:'10vw',height:'10vw',marginLeft:30}} /></span>
-        </div>
+        </div> */}
       </div>
     );
   }
