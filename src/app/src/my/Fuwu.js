ebca4147a575bb3b793c2b9426dab2e902c8fb77
2334168774 <2334168774@qq.com>
2019-12-13 19:00:06 +0800

    update

--- src/app/src/my/Fuwu.js
+++ src/app/src/my/Fuwu.js
@@ -1,17 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router,Route,Link} from 'react-router-dom';
-import { NavBar, Icon, Tabs ,Carousel} from 'antd-mobile';
-export default class Fuwu extends Component {
-  render() {
-    return (
-      <div>
-         <NavBar
-            style={{ width:'100%',backgroundColor:'#37376f',color:'#fff',position:'fixed ',top:'0',zIndex:1,height:'7vh'}}
-            leftContent={[
-                <Link to="/shezhi"><span style={{fontSize:'17px',color:'white'}} className="iconfont icon-ico_leftarrow"></span></Link>
-            ]}
-            >
-            <span>服务协议和隐私政策</simport React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import { NavBar} from 'antd-mobile';
 export default class Fuwu extends Component {
@@ -66,12 +53,6 @@ export default class Fuwu extends Component {
 
               </div>
             </div>
-      </div>
-    );
-  }
-}
-pan>
-        </NavBar>
       </div>
     );
   }
