16491d0945da9d3c9da625574b4d600316f86d35
aalixue <2319684501@qq.com>
2019-12-14 16:07:29 +0800

    .

---
+++ src/server/routes/major.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var con = require('./postgreSQL');
+
+router.get('/list', function (req, res) {
+  let sql = 'select * from major';
+  con.query(sql, [], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result.rows);
+    }
+  })
+});
+
+module.exports = router;
