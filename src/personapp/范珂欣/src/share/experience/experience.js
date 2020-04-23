75e1fc2900c36ba7414bb91f11fcc9326ffe6879
Fankexin <974094944@qq.com>
2020-04-23 12:48:07 +0800

    .

---
+++ src/personapp/范珂欣/src/share/experience/experience.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View, ScrollView, SafeAreaView, TextInput, Dimensions, ImageBackground, Image, TouchableOpacity, AsyncStorage, Alert, DeviceEventEmitter } from 'react-native';
+
+export default class experience extends Component {
+    render() {
+        return (
+            <View>
+                <Text>2</Text>
+            </View>
+        )
+    }
+}
